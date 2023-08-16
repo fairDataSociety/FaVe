e20298eedb0c183e8a642ba1398b2ef74b16f319
asabya <asabya@gmail.com>
2023-08-16 12:50:24 +0530

    cleanup

--- adapters/handlers/collections.go
+++ adapters/handlers/collections.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"fmt"
 	"github.com/fairDataSociety/FaVe/pkg/document"
 	"github.com/fairDataSociety/FaVe/restapi/operations"
 	"github.com/fairdatasociety/fairOS-dfs/pkg/collection"
@@ -36,7 +35,7 @@ func (s *Handler) FaveCreateCollectionHandler(request operations.FaveCreateColle
 			return operations.NewFaveCreateCollectionBadRequest().WithPayload(createErrorResponseObject("Wrong indexes type"))
 		}
 	}
-	fmt.Println(indexes)
+
 	col := &document.Collection{
 		Name:    collectionRaw.Name,
 		Indexes: indexes,
