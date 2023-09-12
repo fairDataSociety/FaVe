3bc943bdfb3e849f825da5758d008675d1ee63d8
asabya <asabya@gmail.com>
2023-09-12 18:26:54 +0530

    test goreleaser docker build 2

---
+++ version.go
@@ -0,0 +1,14 @@
+package FaVe
+
+var (
+	version = "dev"
+	commit  string
+
+	// Version is used to print dfs version
+	Version = func() string {
+		if commit != "" {
+			return version + "-" + commit
+		}
+		return version
+	}()
+)
